1141e131c66fbb34ef8e66e272d93651acebb451
LAPTOP-3PRIMGIA\tina <h1202319008@hiratagakuen.onmicrosoft.com>
2024-06-24 10:26:03 +0900

    vs2020からvs2019に変更。 プレイヤー移動途中。

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -1,35 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-//プレイヤーの左右移動＋ジャンプ
 public class Player : MonoBehaviour
 {
-    Vector3 mouse_pos, world_pos;//座標用の変数
+    public GameObject target;
+    public float speed;
+    public bool isTouch;
 
     #region 参考サイト
     /* 参考サイト
     オブジェクトをクリックした座標へ移動させる
-    https://tanisugames.com/mouse-move/ */
+    https://futabazemi.net/unity/click_xpos_move */
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 0.1f;
+        target = GameObject.Find("target_obj");
+        isTouch = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "target_obj")
+        {
+            isTouch = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        mouse_pos = Input.mousePosition;//マウス座標の取得
-        world_pos = Camera.main.ScreenToWorldPoint(new Vector3(mouse_pos.x, mouse_pos.y, 10.0f));//スクリーン座標をワールド座標に変換
-        transform.position = world_pos;//ワールド座標を自身の座標に設定
-    }
-
-    private void Move()
-    {
-
+        if (isTouch == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+        }
     }
 }
