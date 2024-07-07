103bf863a45216af9c6e1dc6d7c21f46d5befed3
LAPTOP-3PRIMGIA\tina <h1202319008@hiratagakuen.onmicrosoft.com>
2024-07-07 09:53:34 +0900

    エネミーランダム生成＋10体エネミー出現追加。

---
+++ Assets/Enemy/FromAboveEnemyFormation.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//一定の距離でエネミーを出現
+public class FromAboveEnemyFormation : MonoBehaviour
+{
+    public GameObject[] enemy_obj;//エネミーオブジェクト取得
+    private int rnd;//ランダム生成
+
+    #region 参考サイト
+    /* 参考サイト
+    コルーチンの使い方
+    https://umistudioblog.com/coroutinehowto/ */
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rnd = Random.Range(0, 2);//ランダム生成(最小値, 最大値-1の値)
+        Debug.Log("ランダム値 = " + rnd);//ランダム生成値の確認
+        StartCoroutine(EnemyDown());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator EnemyDown()
+    {
+        if(rnd == 0)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //上から落ちてくるエネミーの生成
+                Instantiate(enemy_obj[0], new Vector3(-16.0f + (i * 4.0f), 8.5f, 0.0f), Quaternion.identity);
+                //1つ生成したら待つ
+                yield return new WaitForSeconds(1.0f);//何秒待つか
+            }
+        }
+        else if (rnd == 1)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //上から落ちてくるエネミーの生成
+                Instantiate(enemy_obj[0], new Vector3(16.0f - (i * 4.0f), 8.5f, 0.0f), Quaternion.identity);
+                //1つ生成したら待つ
+                yield return new WaitForSeconds(1.0f);//何秒待つか
+            }
+        }
+    }
+}
--- Assets/Enemy/FromAbove_Enemy.cs
+++ Assets/Enemy/FromAbove_Enemy.cs
@@ -6,28 +6,22 @@ using UnityEngine;
 //エネミーのRigidbody2Dの「Constraints」の「Freeze Rotation」の z にチェックを入れる（回転を防ぐため）
 public class FromAbove_Enemy : MonoBehaviour
 {
-    //public GameObject line_obj;//エネミーラインを取得→使用しないかもしれない
     public float enemyspeed;//落下速度
     public bool isTouch;//接触フラグ
-
-    //ランダムに表示させるための変数↓↓
-    public Vector2 AreaMin = new Vector2(-16, 0);//最小位置
-    public Vector2 AreaMax = new Vector2(16, 0);//最大位置
 
     private Vector2 enemypos;//エネミー位置
 
     // Start is called before the first frame update
     void Start()
     {
-        enemyspeed = 0.05f;//落下速度
+        enemyspeed = 0.1f;//落下速度
         isTouch = false;//接触フラグ
     }
 
     // Update is called once per frame
     void Update()
     {
-        //揺れる演出処理
-        FromAboveEnemyMove();//本体処理
+        FromAboveEnemyMove();
     }
 
     //落下物本体
@@ -37,7 +31,7 @@ public class FromAbove_Enemy : MonoBehaviour
         if (isTouch)
         {
             enemyspeed = 0.0f;//エネミーを停止させる
-            Destroy(gameObject, 1.5f);//エネミー削除
+            Destroy(gameObject, 1.0f);//エネミー削除
         }
         else
         {
@@ -46,7 +40,6 @@ public class FromAbove_Enemy : MonoBehaviour
             //エネミー位置のy軸の変更
             this.transform.position = new Vector2(enemypos.x, enemypos.y - enemyspeed);
         }
-
     }
 
     //地面との衝突判定
