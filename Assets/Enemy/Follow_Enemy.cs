c39b43eb45da8090fdca014f13d7e851ad5f1b46
DESKTOP-3GUQ93R\GA2A <a>
2024-07-29 15:22:51 +0900

    左右からくるエネミー：途中でプレイヤー追従停止追加。

--- Assets/Enemy/Follow_Enemy.cs
+++ Assets/Enemy/Follow_Enemy.cs
@@ -8,7 +8,8 @@ public class Follow_Enemy : MonoBehaviour
     //インスペクター(オブジェクトがアクティブか非アクティブか見るにはインスペクターにする必要がある)
     [SerializeField] public GameObject player_obj;//プレイヤーを取得(プレイヤーの位置を保存するため)
     public float enemyspeed;//落下速度
-    
+    public bool isTouch;//接触フラグ
+
     private float rnd;//ランダム生成
 
     #region 参考サイト
@@ -25,7 +26,7 @@ public class Follow_Enemy : MonoBehaviour
     {
         enemyspeed = 0.05f;//エネミー速度
         //player_obj = GameObject.Find("player");//オブジェクトの取得
-        //isTouch = false;
+        isTouch = false;//接触フラグ
         //isScreen = false;//画面内 false
         rnd = Random.Range(-3.0f, 8.5f);//ランダム生成(最小値, 最大値-1の値)
         transform.position = new Vector2(transform.position.x, rnd);//エネミーはランダムに表示
@@ -46,20 +47,27 @@ public class Follow_Enemy : MonoBehaviour
         //プレイヤーがアクティブの場合
         if(player_obj)
         {
-            //向きたい方向を計算(プレイヤー位置 - オブジェクト位置)
-            Vector3 dir = (player_obj.transform.position - gameObject.transform.position);
-            //プレイヤーに追従するエネミーの向きを回転
-            //Quaternion 回転を表すもの FromToRotation(開始方向, 終了方向)
-            this.transform.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+            if (transform.position.y > -5.0f)
+            {
+                //プレイヤー座標にエネミーの座標に変換
+                //MoveTowards(移動したいオブジェクトの位置, ターゲットの位置, 移動速度)
+                transform.position = Vector3.MoveTowards(transform.position, player_obj.transform.position, enemyspeed);
+            }
+        }
+        //接触フラグが真になった場合
+        if(isTouch)
+        {
+            Destroy(gameObject, 1.0f);
+        }
+    }
 
-            //プレイヤーとエネミーの距離を図る→プレイヤー範囲内だった場合エネミーの追従を行わない
-
-
-            //プレイヤーの位置に移動するのではなく向いた方向に移動する方が良い？
-            //プレイヤー座標にエネミーの座標に変換
-            //MoveTowards(移動したいオブジェクトの位置, ターゲットの位置, 移動速度)
-            transform.position = Vector3.MoveTowards(transform.position, player_obj.transform.position, enemyspeed);
-            //Debug.Log("プレイヤー位置座標 = " + player_obj.transform.position);//プレイヤー位置位置の確認
+    //地面との衝突判定
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //もし地面と衝突した場合
+        if (collision.collider.tag == "Ground")
+        {
+            isTouch = true;//フラグをtrueに返す
         }
     }
 }
