d6f1b5641870bb42de1541d836596045465f4b41
DESKTOP-M30FNO0\GA2A <a>
2024-08-02 15:44:51 +0900

    新規エネミー追加。 壁に当たると跳ね返る。

--- Assets/Animation/fromabove_enemy/enemy/enemyani.cs
+++ Assets/Animation/fromabove_enemy/enemy/enemyani.cs
@@ -23,14 +23,14 @@ public class enemyani : MonoBehaviour
     private IEnumerator EnemyDownAni()
     {
         rnd = Random.Range(0, 2);//ランダム生成(最小値, 最大値-1の値)
-        Debug.Log("ランダム値 = " + rnd);//ランダム生成値の確認
+        //Debug.Log("ランダム値 = " + rnd);//ランダム生成値の確認
         
         if(rnd == 0)
         {
             for (int i = 0; i <= 6; i++)
             {
                 Instantiate(enemy_obj[i], new Vector2(0.0f, 0.0f), Quaternion.identity);
-                yield return new WaitForSeconds(1.5f);
+                yield return new WaitForSeconds(1.0f);
             }
         }
         else
---
+++ Assets/Enemy/BounceEnemyData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceEnemyData : MonoBehaviour
+{
+    [SerializeField] GameObject Bounce_obj;//エネミー取得
+    public Count_Time count_time;//countup の値の取得
+    private float bCount = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //制限時間が1すぎた場合1つ生成
+        bCount = count_time.CountUP;//プロパティの取得
+        Debug.Log(bCount);//ログで確認
+
+        //if(bCount)
+        //{
+        //StartCoroutine(BounceData());//コルーチン開始
+        //}
+    }
+
+    /// <summary>
+    /// 常に動き続けるエネミーの生成
+    /// </summary>
+    /// <returns>エネミー生成時1.0待つ</returns>
+    private IEnumerator BounceData()
+    {
+        Instantiate(Bounce_obj, new Vector2(0.0f, 0.0f), Quaternion.identity);
+        yield return new WaitForSeconds(1.0f);
+    }
+}
